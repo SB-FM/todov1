35f0aced17e9e9b54b2eb35b12ca290ee7e669ef
NiL <superbeast@email.com>
2017-11-01 05:43:27 +0100

    played with e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('todo-list App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display todos header', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('todos');
   });
 });
